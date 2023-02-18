5a8502c8981fb86122349735e63d9b442caa0463
gracygupta <gracy2112004@akgec.ac.in>
2023-02-18 00:29:35 -0800

    v1

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const port = process.env.PORT || 3001;
+const { Server } = require("socket.io");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    // console.log(data);
+  });
+
+  socket.on("send_message", (data) => {
+    // console.log(data);
+    var a = true;
+    if (a) {
+      socket.to(data.room).emit("receive_message", data);
+      a = false;
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected:", socket.id);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
